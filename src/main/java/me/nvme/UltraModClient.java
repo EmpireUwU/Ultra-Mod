973bd666ced0217b952f8374d34286e77c876086
Empire420 <adamrubes@email.cz>
2025-06-07 17:35:49 +0200

    Another setup

---
+++ src/main/java/me/nvme/UltraModClient.java
@@ -0,0 +1,10 @@
+package me.nvme;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltraModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
