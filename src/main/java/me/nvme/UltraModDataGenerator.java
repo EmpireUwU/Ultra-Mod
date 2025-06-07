c951371d49521bbc47cc7e49d4d24977b7dcc40d
Empire420 <adamrubes@email.cz>
2025-06-07 17:17:49 +0200

    init commit

---
+++ src/main/java/me/nvme/UltraModDataGenerator.java
@@ -0,0 +1,11 @@
+package me.nvme;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UltraModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
